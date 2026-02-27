dad6a51e1e5a674028ca249c424172005d461818
Rzhvms <160605m@mail.ru>
2026-02-27 23:51:42 +0500

    add DefaultApi

---
+++ Apps/DefaultApi/Api/Program.cs
@@ -0,0 +1,24 @@
+using LoggingLib;
+
+namespace Api;
+
+/// <summary>
+/// Точка входа в приложения
+/// </summary>
+public sealed class Program
+{
+    /// <summary>
+    /// Запуск приложения
+    /// </summary>
+    public static void Main(string[] args)
+    {
+        Host.CreateDefaultBuilder(args)
+            .UseInfraSerilog()
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            })
+            .Build()
+            .Run();
+    }
+}
